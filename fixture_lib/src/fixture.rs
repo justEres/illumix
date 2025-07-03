e4a59776d3bc2e9ebea701600f1d175b935e8df7
Eres <erik.tschoepe@gmail.com>
2025-07-03 22:30:19 +0200

    Created library for managing fixture universe states

---
+++ fixture_lib/src/fixture.rs
@@ -0,0 +1,73 @@
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Serialize,Deserialize)]
+pub struct Fixture{
+    id: u8,
+    pub dmx_address: u16,
+    components: Vec<FixtureComponent>,
+}
+
+impl Fixture{
+    pub fn new(id: u8, dmx_address: u16) -> Fixture{
+        Fixture { id, dmx_address, components: Vec::new() }
+    }
+    pub fn add_component(&mut self, component: FixtureComponent){
+        self.components.push(component);
+    }
+    pub fn get_dmx_values(&self) -> Vec<u8>{
+        let mut dmx_values = Vec::new();
+        for component in &self.components{
+            dmx_values.append(&mut component.get_dmx_values());
+        }
+        return dmx_values;
+    }
+
+}
+
+#[derive(Serialize,Deserialize)]
+pub enum FixtureComponent{
+    Color(Color),
+    Dimmer(Dimmer),
+    Position(Position),
+}
+
+impl FixtureComponent{
+    fn get_dmx_values(&self) -> Vec<u8>{
+        match self{
+            FixtureComponent::Color(c) => {
+                vec![c.r,c.g,c.b]
+            }
+            FixtureComponent::Dimmer(d) => {
+                vec![d.intensity]
+            }
+            FixtureComponent::Position(p) => {
+                let pan_lower = (p.pan & 0xFF) as u8;
+                let pan_higher = (p.pan >> 8) as u8; 
+                let tilt_lower = (p.tilt & 0xFF) as u8;
+                let tilt_higher = (p.tilt >> 8) as u8; 
+                vec![pan_higher,pan_lower,tilt_higher,tilt_lower]
+            }
+        }
+    }
+}
+
+
+#[derive(Serialize,Deserialize,Default)]
+pub struct Color{
+    pub r: u8,
+    pub g: u8,
+    pub b: u8
+}
+
+#[derive(Serialize,Deserialize,Default)]
+pub struct Dimmer{
+    pub intensity: u8,
+}
+
+#[derive(Serialize,Deserialize,Default)]
+pub struct Position{
+    pub pan: u16,
+    pub tilt: u16,
+}
+
--- fixture_lib/src/lib.rs
+++ fixture_lib/src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+
+mod fixture;
+mod universe;
+
+
 
 #[cfg(test)]
 mod tests {
+    use std::{fs::File, future, io::Write};
+
+    use crate::{fixture::{Color, Dimmer, Fixture}, universe::Universe};
+
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn gen_dmx_universe(){
+        let mut universe = Universe::new();
+
+        let mut led = Fixture::new(0, 1);
+
+        led.add_component(fixture::FixtureComponent::Color(Color{r:255,g:03,b:100}));
+        led.add_component(fixture::FixtureComponent::Dimmer(Dimmer { intensity: 50 }));
+
+        universe.add_fixture(led);
+
+        let mut file = File::create("test_universe.json").expect("Couldnt open file");
+        file.write_all(universe.export_to_json().as_bytes()).expect("Couldnt write to file");
+        dbg!(universe.get_dmx_values());
     }
 }
