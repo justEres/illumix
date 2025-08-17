0d70b69832f07abfed01171841d4a134f36e41e3
Theo <theo.petermann07@gmail.com>
2025-08-17 16:03:37 +0000

    Setup new Fader Page

---
+++ egui_frontend/src/ui_elements/fader.rs
@@ -0,0 +1,28 @@
+
+
+pub struct Fader{
+    pub pos_x: f32,
+    pub pos_y: f32,
+
+    pub fader_value: u8,
+
+    pub fixture_id: Option<u8>,
+    pub fixture_component_index: Option<u8>,
+}
+
+impl Fader{
+    pub fn new(pos_x: f32, pos_y: f32, fixture_id: Option<u8>, fixture_component_index: Option<u8>) -> Self{
+
+
+        Self{
+            pos_x,
+            pos_y,
+            
+            fader_value: 0,
+
+            fixture_id,
+            fixture_component_index,
+        }
+
+    }
+}
