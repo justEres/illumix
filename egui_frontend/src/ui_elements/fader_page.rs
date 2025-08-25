68c930dc77788b18137e22269d02f3634c827c17
Theo <theo.petermann07@gmail.com>
2025-08-25 15:23:30 +0000

    Updated Auto Sclling for Fader Page

--- egui_frontend/src/ui_elements/fader_page.rs
+++ egui_frontend/src/ui_elements/fader_page.rs
@@ -1,13 +1,19 @@
 use std::fmt::format;
 
+
 use eframe::{CreationContext, Frame};
 use eframe::egui::{
     self, Button, CentralPanel, ColorImage, Context, Slider, TextureHandle, Ui, UiBuilder, Vec2, Window
 };
 use eframe::egui::{Color32, Pos2, Rect, Style};
 use wasm_bindgen::prelude::wasm_bindgen;
+use web_sys::js_sys::Intl::DateTimeFormat;
 
 use crate::fader_page::fader::Fader;
+use crate::fader_page::ui_auto_scalling::AutoScaller;
+
+#[path = "ui_helper/ui_auto_scalling.rs"]
+mod ui_auto_scalling;
 
 mod fader;
 
@@ -16,6 +22,8 @@ pub struct FaderPage {
 
     //Styling
     panel_resolution: Vec2,
+
+    ui_auto_scaller: ui_auto_scalling::AutoScaller,
 
     rect: Rect,
 }
@@ -28,24 +36,26 @@ impl FaderPage {
 
         let rect = Rect::from_min_size(Pos2::ZERO, egui::vec2(0.0, 0.0));
 
+
         Self {
             fader_list,
             panel_resolution,
+            ui_auto_scaller: AutoScaller::new(),
             rect,
         }
     }
 
     pub fn show(&mut self, ctx: &egui::Context) {
-        CentralPanel::default().show(ctx, |ui| {
+        
 
             egui::CentralPanel::default().show(ctx, |ui| {
-
+                ctx.request_repaint();
                 self.rect = ui.max_rect();
                 self.panel_resolution = Vec2 { x: self.rect.max.x / 100., y: self.rect.max.y / 100.};
 
                 let mut style = (*ctx.style()).clone();
-                style.spacing.slider_width = ((self.panel_resolution.y) * 37.5); // Wider slider
-                style.spacing.interact_size.y = ((self.panel_resolution.y) * 6.); // Taller handle
+                style.spacing.slider_width = (self.ui_auto_scaller.get_cell_size().y); // Wider slider
+                style.spacing.interact_size.y = (self.ui_auto_scaller.get_cell_size().x / 1.75); // Taller handle
                 style.visuals.handle_shape = egui::style::HandleShape::Rect { aspect_ratio: 1.5 };
     
                 ui.set_style(style.clone());
@@ -53,31 +63,23 @@ impl FaderPage {
 
                 self.draw_slider_bank(ui);
 
+                //self.draw_ctrl_buttons(ui);
+
             });
 
-        });
+        
     }
 
     fn draw_slider_bank(&mut self, ui: &mut Ui){
-
         
 
-        for i in 0..24{
-
-            let mut offset = Vec2 { x: 0., y: 0. };
-            if (i > 5 && i < 12) || (i > 17){
-                offset = Vec2 { x: (self.panel_resolution.x * 6.), y: 0. };
-            }
-
-            if i < 12{
-                offset += Vec2{x: (self.panel_resolution.x * 6.) * i as f32, y:0.};
-            }else{
-                offset += Vec2{x: (self.panel_resolution.x * 6.) * (i as f32 - 12.), y: self.panel_resolution.y * 50.};
-            }
+        for i in 0..24{           
             
 
-            let local = egui::Rect::from_min_size(self.rect.min + offset, egui::vec2(20.0, 20.0));
+            let local = egui::Rect::from_min_size(self.ui_auto_scaller.get_rect(self.rect, true, i as u8).min, egui::vec2(20.0, 20.0));
 
+            
+            
             let response = ui.put(
                 local,
                 egui::Slider::new(&mut self.fader_list[i].fader_value, 0..=255)
@@ -89,24 +91,33 @@ impl FaderPage {
                 self.fader_list[i].fader_value_changed();
             }
 
-            let local = egui::Rect::from_min_size(self.rect.min + offset + Vec2{x:0.,y:self.panel_resolution.y * 40.}, egui::vec2(self.panel_resolution.y * 6., 20.0));
+            let local = self.ui_auto_scaller.get_rect(self.rect, false, i as u8);
+
+            
+
 
             if self.fader_list[i].is_selected == false && ui.put(
                 local,
                 Button::new(format!("{}", i + 1))
             ).clicked(){
+                
                 self.fader_list[i].is_selected = true;
             }else if self.fader_list[i].is_selected == true && ui.put(
                 local,
                 Button::new(format!("{}", i + 1))
             ).clicked(){
-                self.fader_list[i].is_selected =false;
+                self.fader_list[i].is_selected = false;
             }            
-            
+
+            let frame_rect =self.ui_auto_scaller.get_rect(self.rect, false, i as u8);
+
+
+            let local = egui::Rect::from_min_size(
+                frame_rect.min - Vec2{x:(frame_rect.min.x - frame_rect.max.x) / 2.5 ,y:self.panel_resolution.y * -0.4},
+                 egui::vec2(self.panel_resolution.y * 1., 9.)
+                );
 
             
-
-            let local = egui::Rect::from_min_size(self.rect.min + offset + Vec2{x:self.panel_resolution.y * 2.5,y:self.panel_resolution.y * 40.5}, egui::vec2(self.panel_resolution.y * 1., 9.));
 
             ui.allocate_rect(local, egui::Sense::hover());
 
@@ -115,19 +126,20 @@ impl FaderPage {
 
             if self.fader_list[i].is_selected == true{
                 color = Color32::from_rgb(255, 200, 120);
-            }
-
-            ui.painter().rect_filled(
-                local,
-                5.0,                                                // corner radius
-                color, // background color
-            );
-            
-        }
-
+                ui.painter().rect_filled(
+                    local,
+                    5.0,// corner radius
+                    color, // background color
+                );
+            }   
+        }    
         
-            
-        
+    }
+
+    fn draw_ctrl_buttons(&mut self, ui: &mut Ui){
+        let local = self.ui_auto_scaller.get_ctrl_button(0);
+
+        ui.put(local, Button::new("Test"));
     }
 }
 
